9fec74f9f88d59f9cab6c878cc8907a0baa4ce2d
srehan1 <syed.rehan@shoppertrak.com>
2023-12-25 00:58:21 +0530

    Append items to Linked List

--- LinkedList.py
+++ LinkedList.py
@@ -5,7 +5,7 @@ Linked List
 
 class Node:
     """
-    Create Nodes
+    Create Nodes.
     """
 
     def __init__(self, value):
@@ -22,11 +22,19 @@ class LinkedList:
     def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
-        self.tail = None
+        self.tail = new_node
         self.length = 1
 
-    def append(self):
-        pass
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
 
     def prepend(self):
         pass
@@ -34,6 +42,24 @@ class LinkedList:
     def insert(self, index, value):
         pass
 
+    def __str__(self):
+        """
+        Prints content of Linked List created.
+        :return: String of Linked List values
+        """
+        ll_str = ""
+        temp = self.head
+        while temp is not None:
+            ll_str += str(temp.value) + "-->"
+            temp = temp.next
+
+        return ll_str
+
 
 ll = LinkedList(4)
-print(f"Head: {ll.head.value}, Tail: {ll.tail}, Next node: {ll.head.next}, Length: {ll.length}")
+print(f"Head: {ll.head.value}, Tail: {ll.tail.value}, Next node: {ll.head.next}, Length: {ll.length}")
+print(f"Linked List: {ll}")
+print("Adding new nodes...")
+ll.append(1)
+ll.append(2)
+print(f"After appending new nodes: {ll}")
